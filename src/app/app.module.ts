70c226f2e8d6088be311de326730c7ad1c042515
kaung <kaungmyathun7@gmail.com>
2023-04-22 10:34:12 +0630

    learn: new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { StepOneComponent } from './step-one/step-one.component'
+import { StepOneComponent } from './step-one/step-one.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component'
 
 const routes : Routes =[
+  {path: '' , component:HomePageComponent},
   {path: 'st-one', component: StepOneComponent},
+  {path: '**', component: PageNotFoundComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     StepOneComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
