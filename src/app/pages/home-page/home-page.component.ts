1dec563e6eb9e1bc32f2495b9683069ab96e2d1b
kaung <kaungmyathun7@gmail.com>
2023-04-22 17:18:00 +0630

    testing: function parameter

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -12,4 +12,35 @@ export class HomePageComponent1 {
   addFun(){
     this.count ++
   }
+
+  sayHello(){
+    window.alert('Hello User')
+  }
+
+  mark=0;
+
+  addMark(){
+    this.mark ++
+  }
+  minusMark(){
+    if (this.mark == 0) {
+      window.alert("sorry cannot reduce mark anymore")
+    }else{
+      this.mark --
+    }
+  }
+
+  addByNumber(number:number){
+    this.mark += number
+  }
+
+  minusByNumber(number:number){
+    if (this.mark >= 5) {
+      this.mark -= number
+    }else if (this.mark < 5 && this.mark >0) {
+      window.alert("sorry your number is less than 5")
+    }else{
+      window.alert('sorry you cannot reduce less than 0')
+    }
+  }
 }
