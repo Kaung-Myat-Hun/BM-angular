8341d40d6dea5d88fbbf453c3740d8acf921d707
kaung <kaungmyathun7@gmail.com>
2023-04-22 17:06:51 +0630

    learn: function angular

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent1 {
-
+  count = 0;
+  addFun(){
+    this.count ++
+  }
 }
