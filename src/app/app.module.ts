c269e9aa81e6cceda1a172a413748fe14d24c5c7
kaung <kaungmyathun7@gmail.com>
2023-04-22 10:24:05 +0630

    learning angular route and testing own from start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { StepOneComponent } from './step-one/step-one.component'
+
+const routes : Routes =[
+  {path: 'st-one', component: StepOneComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepOneComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
