11976e0febfc9b0cb32d4a967ce72ca6267abdd8
kaung <kaungmyathun7@gmail.com>
2023-05-02 16:11:14 +0630

    update

--- src/app/sizer/sizer.component.ts
+++ src/app/sizer/sizer.component.ts
@@ -10,8 +10,8 @@ export class SizerComponent {
   @Input() size! : number | string;
   @Output() sizeChange = new EventEmitter<number>();
 
-  dec() { this.resize(-1)}
-  inc() {this.resize(+1)}
+  dec() { this.resize(-1) }
+  inc() { this.resize(+1) }
 
   resize(delta: number){
     this.size = Math.min(40, Math.max(8, +this.size+ delta))
